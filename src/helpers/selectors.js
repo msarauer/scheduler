e951fab6511e7beb67e7755b22205c0a5aa0db33
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-09 17:07:26 -0600

    added getAppointmentsForDay helper function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,12 @@
+export function getAppointmentsForDay(selector, day) {
+  const dayObj = selector.days.filter(item => (day === item.name));
+  if(dayObj[0]) {
+    const appointmentArray = dayObj[0].appointments;
+    const result = appointmentArray.map((item) => {
+      return selector.appointments[item];
+    })
+    return result;
+  }
+  return [];
+};
+
