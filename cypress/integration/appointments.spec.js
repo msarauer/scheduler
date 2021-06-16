c104fc4c859ef766c3be117fa02fc98b41deddd0
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-16 15:15:41 -0600

    added first cypress tests

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,44 @@
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
+    cy.get("[alt=Add]").first().click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt='Edit']").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it.only("should cancel an interview", () => {
+    // Clicks the delete button for the existing appointment
+    cy.get("[alt='Delete']").first().click({ force: true });
+    // Clicks the confirm button
+    cy.contains("Confirm").click();
+    // Sees that the appointment slot is empty
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+  });
+});
